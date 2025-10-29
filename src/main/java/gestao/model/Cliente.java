476d908897133a16932b6cba04665b67eb77e293
Matheus Zacarias <matheuus412@gmail.com>
2025-10-29 12:47:10 -0300

    feat: listar cliente

---
+++ src/main/java/gestao/model/Cliente.java
@@ -0,0 +1,88 @@
+package gestao.model;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private String endereco;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nome, String email, String telefone, String cpf, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
+}
--- src/main/java/gestao/servlet/ClientServlet.java
+++ src/main/java/gestao/servlet/ClientServlet.java
@@ -1,5 +1,7 @@
 package gestao.servlet;
 
+import gestao.model.Cliente;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -40,6 +42,14 @@ public class ClientServlet extends HttpServlet {
 
     private void listarClientes(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        Cliente client1 = new Cliente(1, "João Silva", "joao@email.com", "112223333", "123.456.789-00", "Rua A, 123");
+        Cliente client2 = new Cliente(2, "Maria Santos", "maria@email.com", "987654321", "987.654.321-00", "Rua B, 456");
+
+        List<Cliente> clientes = List.of(client1, client2);
+
+        request.setAttribute("clientes", clientes);
+
         request.getRequestDispatcher("/lista-clientes.jsp").forward(request, response);
     }
 }
